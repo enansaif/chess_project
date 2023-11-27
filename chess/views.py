f23e209a3ac27aa020b20978d1c34acedc3ff7fa
rect1fy <enansaifme33@gmail.com>
2023-11-27 16:29:48 +0600

    added a home view

--- chess/views.py
+++ chess/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
     html = "<html><body>Hello cruel world</body></html>"
-    return HttpResponse(html)
+    return render(request, 'chess/index.html')
