bd020a3c6847c299a236e19b4d511bd2077acd5a
enansaif <enansaifme33@gmail.com>
2023-12-04 19:53:13 +0600

    removed django channels and daphne from project reverted to default django app

--- chess/views.py
+++ chess/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-# Create your views here.
 
 def home(request):
-    html = "<html><body>Hello cruel world</body></html>"
     return render(request, 'chess/index.html')
