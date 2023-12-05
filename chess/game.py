fb01170b105dd2da392536cea6604b5eca7e59ca
enansaif <enansaifme33@gmail.com>
2023-12-05 15:06:32 +0600

    Wrote ai random move generation logic

---
+++ chess/game.py
@@ -0,0 +1,18 @@
+import chess
+import random
+import time
+board = chess.Board()
+
+def ai_next_move(player1_move:str) -> list:
+    '''Function receives a valid move from player1, ai makes a move 
+       and returns a list of next legal moves player can make.'''
+    
+    board.push(chess.Move.from_uci(player1_move))
+    
+    ai_move = random.choice(list(board.legal_moves))
+    board.push(ai_move)
+    
+    next_legal_moves = ' '.join([str(move) for move in board.legal_moves])
+    return str(ai_move), next_legal_moves
+
+print(ai_next_move("g1f3"))
